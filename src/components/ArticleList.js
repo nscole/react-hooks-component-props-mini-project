2112ce7eec6ce5506c5455ac6c3572055f24d9a9
Naomi Cole <naomisarahcole@gmail.com>
2022-07-18 18:34:33 +1000

    Completed Assignment

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,13 +1,19 @@
 import React from "react";
-import Article from "./Article";
+import Article from "./Article.js";
 
 
-function ArticleList (posts) {
-    return <main>
+function ArticleList ({posts}) {
+    const ArticleArray = posts.map((post) => (
         <Article
-        
-        
-        />
+            key={posts.id}
+            title={posts.title}
+            date={posts.date}
+            preview={posts.preview}
+            minutes={posts.minutes}
+            />
+    ))
+    return <main>
+        {ArticleArray}
     </main> 
 }
 
