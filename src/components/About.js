015ff1c6dc3b910583e429824eed2c1a62c0071a
Naomi Cole <naomisarahcole@gmail.com>
2022-07-18 17:29:53 +1000

    5 passed

---
+++ src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function About (props) {
+    return <aside>
+        <img src={props.image} alt="blog logo"></img>
+        <p>{props.about}</p>
+    </aside>
+}
+
+
+export default About;
--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Header from "./Header.js";
+import About from "./About.js";
 import blogData from "../data/blog";
 
 console.log(blogData);
 
 function App() {
+     
   return (
+    
     <div className="App">
+      <Header/>
+      <About image={blogData.image} about={blogData.about}/>
       You're on your own from here! Follow the deliverables; test things out in
       the browser as you write your code; and good luck!
     </div>
